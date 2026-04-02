db6cae70230acfbb052928871cb6c7d67956ab77
diegonunezsdg2 <diego.nunez.martin@alumnos.upm.es>
2026-04-02 17:28:40 +0200

    Repositorio registro accesos

---
+++ ISST_Tourist_Cocoon_G13/BACKEND/src/main/java/tourist_cocoon/repository/RegistroAccesoRepository.java
@@ -0,0 +1,11 @@
+package tourist_cocoon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tourist_cocoon.model.RegistroAcceso;
+
+import java.util.List;
+
+public interface RegistroAccesoRepository extends JpaRepository<RegistroAcceso, Long> {
+
+    List<RegistroAcceso> findByHuespedIdOrderByFechaHoraDesc(Long huespedId);
+}
